d0307be94a4f22c41ae0216371ba66a545e77d2f
Dante Lacuadra <55060638+dantelacu2@users.noreply.github.com>
2024-02-02 16:47:31 -0500

    simplify routing to just one page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,8 +8,8 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/schedule' ? 'page' : undefined}>
-				<a href="/schedule">Scheduling</a>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Scheduling</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
