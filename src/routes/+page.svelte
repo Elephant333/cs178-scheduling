bfb7503844793dfb566e2e00af8ed40a7c77cb71
Dante Lacuadra <55060638+dantelacu2@users.noreply.github.com>
2024-02-03 20:21:51 -0500

    add capability to select time slots that work best

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import Interation from '@event-calendar/interaction';
 	import '@event-calendar/core/index.css';
 	import { v4 as uuidv4 } from 'uuid';
+	import Button from '@smui/button';
 
 	let plugins = [TimeGrid, Interation];
 	let options = {
@@ -13,24 +14,58 @@
 		selectable: true,
 		slotMinTime: '10:00:00',
 		slotMaxTime: '23:00:00',
-		headerToolbar: {start: '', center: '', end: ''},
+		headerToolbar: { start: '', center: '', end: '' },
 		select: (info) => addEvent(info),
-		eventClick: (info) => removeEventById(info.event.id),
+		eventClick: (info) => handleEventClick(info.event.id)
 	};
+	let preferred = false;
+
+	function handleEventClick(id) {
+		if (!preferred) {
+			removeEventById(id);
+		} else {
+			options.events = options.events.map((event) => {
+				if (event.id === id) {
+					return {
+						...event,
+						backgroundColor: event.backgroundColor === 'green' ? 'grey' : 'green'
+					};
+				}
+				return event;
+			});
+		}
+	}
 
 	function addEvent(info) {
 		// Assignment is what triggers a refresh to the DOM
-		options.events = [...options.events, { id: uuidv4(), ...info }];
+		options.events = [
+			...options.events,
+			{ id: uuidv4(), selectBackgroundColor: 'grey', backgroundColor: 'grey', ...info }
+		];
 	}
 
 	function removeEventById(id) {
-		options.events = options.events.filter(event => event.id !== id);
+		options.events = options.events.filter((event) => event.id !== id);
 	}
 </script>
 
+<div>
+	<Button
+		variant="unelevated"
+		color={preferred ? 'primary' : 'secondary'}
+		on:click={() => (preferred = !preferred)}>Select Preferred Slots</Button
+	>
+</div>
 <Calendar {plugins} {options} />
 
 <svelte:head>
 	<title>Schedule</title>
 	<meta name="schedule" content="schedule" />
 </svelte:head>
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+	}
+</style>
