43462efb89a9a9a38324d8f6acc63c818450d86a
Nathan Li <moncskoli@gmail.com>
2024-02-05 19:29:34 -0500

    default to preferred

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,45 +15,26 @@
 		slotMinTime: '10:00:00',
 		slotMaxTime: '23:00:00',
 		headerToolbar: { start: '', center: '', end: '' },
-		selectBackgroundColor: 'grey',
+		selectBackgroundColor: 'green',
+		eventTextColor: 'white',
 		select: (info) => addEvent(info),
-		eventClick: (info) => handleEventClick(info.event.id),
+		eventClick: (info) => removeEventById(info.event.id),
 		eventDrop: (info) => handleEventUpdate(info),
 		eventResize: (info) => handleEventUpdate(info),
 	};
-	let preferred_slots = false;
-	let last_resort_slots = false;
+	let preferred_slots = true;
 	let copied = [];
 	let copiedDay = 0;
-	let copyButtons = Array(7).fill(true); // false means copy, true means paste
+	let copyButtons = Array(7).fill(true);
 	
 	function addEvent(info) {
 		// Assignment is what triggers a refresh to the DOM
-		let color = 'grey'
-		if (preferred_slots || last_resort_slots) {
-			color = last_resort_slots ? '#e6e600' : 'green';
-		}
+		let color = preferred_slots ? 'green' : '#e6e600';
+		let textColor = preferred_slots ? 'white' : 'black';
 		options.events = [
 			...options.events,
-			{ id: uuidv4(), backgroundColor: color, ...info }
+			{ id: uuidv4(), backgroundColor: color, textColor: textColor, ...info }
 		];
-	}
-	
-	function handleEventClick(id) {
-		if (!preferred_slots && !last_resort_slots) {
-			removeEventById(id);
-		} else {
-			options.events = options.events.map((event) => {
-				if (event.id === id) {
-					let color = last_resort_slots ? '#e6e600' : 'green';
-					return {
-						...event,
-						backgroundColor: event.backgroundColor === color ? 'grey' : color
-					};
-				}
-				return event;
-			});
-		}
 	}
 	
 	function handleEventUpdate(info) {
@@ -77,23 +58,14 @@
 	
 	function handleButtonClick(isPreferred) {
 		if (isPreferred) {
-			preferred_slots = !preferred_slots;
-			if (preferred_slots) {
-				last_resort_slots = false;
-				options.selectBackgroundColor = 'green';
-			}
-			else {
-				options.selectBackgroundColor = 'grey';
-			}
-		} else {
-			last_resort_slots = !last_resort_slots;
-			if (last_resort_slots) {
-				preferred_slots = false;
-				options.selectBackgroundColor = '#e6e600';
-			}
-			else {
-				options.selectBackgroundColor = 'grey';
-			}
+			options.selectBackgroundColor = 'green';
+			options.eventTextColor = 'white';
+			preferred_slots = isPreferred;
+		}
+		else {
+			options.selectBackgroundColor = '#e6e600';
+			options.eventTextColor = 'black';
+			preferred_slots = isPreferred;
 		}
 	}
 
@@ -122,14 +94,14 @@
 			let event = JSON.parse(JSON.stringify(copied[i]));
 			let start = new Date(event.start);
 			let end = new Date(event.end);
-	
+
 			start = new Date(start.setDate(start.getDate() + (day - copiedDay)))
 			end = new Date(end.setDate(end.getDate() + (day - copiedDay)));
 	
 			event.start = start;
 			event.end = end;
 			event.id = uuidv4();
-	
+
 			options.events = [
 				...options.events, event
 			];
@@ -147,7 +119,7 @@
 		on:click={() => handleButtonClick(true)}>Select Preferred Slots</Button
 	>
 	<Button
-		class={last_resort_slots ? 'button-yellow' : ''}
+		class={preferred_slots ? '' : 'button-yellow'}
 		variant="unelevated"
 		color="secondary"
 		on:click={() => handleButtonClick(false)}>Select Last Resort Slots</Button
